0cdd8c66f2992d1eabed1cd15d0e70b7393c3424
Viktar Brutsis <viktarbrutsis@gmail.com>
2022-08-09 20:46:30 +0300

    consult done

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider__photos').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg" alt="right"></button>'
+    });
+    
+  });
