7e0c51029680a1cb2d586c8d52213322819675d4
Viktar Brutsis <viktarbrutsis@gmail.com>
2022-08-16 12:17:52 +0300

    added tabs

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,22 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg" alt="left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg" alt="right"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg" alt="right"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            }
+        ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
     
   });
