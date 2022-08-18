38f924af448694683583e8a1302c77938661ca56
Viktar Brutsis <viktarbrutsis@gmail.com>
2022-08-18 20:05:35 +0300

    added modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -20,5 +20,49 @@ $(document).ready(function(){
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
+    // $('.catalog-item__link').each(function(i) {
+    //   $(this).on('click', function(e) {
+    //     e.preventDefault();
+    //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //   });
+    // });
+
+    // $('.catalog-item__back').each(function(i) {
+    //   $(this).on('click', function(e) {
+    //     e.preventDefault();
+    //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //   });
+    // });
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        });
+      });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
     
+    //Modal
+    $('[data-modal=consult]').on('click', function(){
+      $('.overlay, #consult').fadeIn();
+    });
+
+    $('.modal-close').on('click', function() {
+      $('.overlay, #consult, #buy, #thanks').fadeOut();
+    });
+
+    $('.catalog-item__btn').each(function(i) {
+      $(this).on('click', function() {
+        $('#buy .modal-subtitle').text($('.catalog-item__title').eq(i).text());
+        $('.overlay, #buy').fadeIn();
+      });
+    });
   });
+
